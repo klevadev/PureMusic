8b81a7b6764667a001ba6e8e75ba8ccfc3913f9f
Lev Kolesnikov <nickname.text@yandex.ru>
2021-10-14 12:11:22 +0300

    Initial Commit

---
+++ PureMusic/Services/NetworkService.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkService.swift
+//  PureMusic
+//
+//  Created by Lev Kolesnikov on 13/09/2019.
+//  Copyright © 2019 Lev Kolesnikov. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+protocol Networking {
+    func fetchTracks(searchText: String, completion: @escaping (SearchResponse?) -> Void)
+}
+
+class NetworkService: Networking {
+    
+    func fetchTracks(searchText: String, completion: @escaping (SearchResponse?) -> Void) {
+        let parameters = ["term" : "\(searchText)",
+            "limit" : "10",
+            "media" : "music"]
+
+        AF.request(API.url, method: .get, parameters: parameters, encoding: URLEncoding.default).response {
+            (dataResponse) in
+            if let error = dataResponse.error {
+                print("Error recevied requesting data \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let objects = try decoder.decode(SearchResponse.self, from: data)
+                completion(objects)
+                
+            } catch let jsonError {
+                print("Failed to decode JSON", jsonError)
+                completion(nil)
+            }
+        }
+    }
+    
+}
